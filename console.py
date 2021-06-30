148bce1ef801b02f6f1d663d5b11e238bcc84c85
JuanDAC <juanavila12.0308@gmail.com>
2021-06-30 03:23:12 -0400

    [style] (add new styles)

--- console.py
+++ console.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+""" command line insterprete of a AirBnB """
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    command line interpreter handler to loop and interact within classes.\n
+    """
 
     prompt = "(hbnb) "
 
     def emptyline(self):
-        """ Handles the event when entering an empty line. """
+        """ handles the event when entering an empty line. """
         pass
 
     def do_quit(self, line):
@@ -19,6 +23,7 @@ class HBNBCommand(cmd.Cmd):
         """ \033[32m ├»\033[92m Exits the HBNBCommand.\n \033[0m"""
         print("")
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
