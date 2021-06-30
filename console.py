13fc1e1635a18d3636ad26d0896da5c17f3d9fea
JuanDAC <juanavila12.0308@gmail.com>
2021-06-30 13:23:49 -0400

    [advance] (6 task point one)

--- console.py
+++ console.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ command line insterprete of a AirBnB """
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -13,6 +14,38 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ handles the event when entering an empty line. """
         pass
+
+    def do_create(self, line):
+        if not line:
+            print("** class name missing **")
+            return
+        try:
+            class_to_intance = eval(line)
+            if issubclass(class_to_intance, BaseModel):
+                instance = class_to_intance()
+                storage.new(instance)
+                storage.save()
+                print(instance.id)
+            else:
+                raise NameError
+        except (NameError):
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        if not line:
+            print("** class name missing **")
+            return
+        try:
+            class_to_intance = eval(line)
+            if issubclass(class_to_intance, BaseModel):
+                instance = class_to_intance()
+                storage.new(instance)
+                storage.save()
+                print(instance.id)
+            else:
+                raise NameError
+        except (NameError):
+            print("** class doesn't exist **")
 
     def do_quit(self, line):
         """ \033[32m ├»\033[92m Exits the HBNBCommand.\n \033[0m"""
